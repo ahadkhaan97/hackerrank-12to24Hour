383669f7b6cf999930802820d383c8620166cd54
ahad <khansahab.ahad@gmail.com>
2022-11-08 21:21:31 +0500

    submitted.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    print(timeConversion("12:00:00PM"))
+}
+
+fun timeConversion(s: String): String {
+    var time = ""
+    val temp = s.split(":")
+    if (s.contains("AM")) {
+        if (temp[0].toInt() == 12) {
+            time += "00"
+        } else {
+            time += temp[0]
+        }
+    } else {
+        if (temp[0].toInt() == 12) {
+            time += "12"
+        } else {
+            time += temp[0].toInt() + 12
+        }
+    }
+
+    time += ":${temp[1]}:${temp[2]}"
+    time = time.substring(0, time.length - 2)
+
+    return time
+}
